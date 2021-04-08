134b3ab992ab6d49246e8b41af76ddfc1b748894
Tarun Sikka <tarun.sikka@rakuten.com>
2021-04-09 02:43:19 +0530

    UI Screens (Welcome,Login,FaceRec, MoodRec)

---
+++ momt/lib/UI/pages/faceDetector.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:momt/UI/pages/moodDetector.dart';
+
+class FaceDetector extends StatelessWidget {
+  void navigateToProfile(BuildContext context) {
+    Navigator.push(context, MaterialPageRoute(builder: (_) => MoodDetector()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //final PageController controller = PageController(initialPage: 0);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("MomT"),
+          backgroundColor: Colors.deepPurple,
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        //floatingActionButtonLocation: FloatingActionButtonLocation.,
+        body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+              new Container(
+                height: 500,
+                child: Image.asset("assets/images/faceId.png"),
+              )
+            ])),
+        floatingActionButton: Container(
+            //width: 100,
+            child: FloatingActionButton.extended(
+          onPressed: () {
+            // Add your onPressed code here!
+            navigateToProfile(context);
+          },
+          label: Text(
+            'Scan to Start',
+            style: TextStyle(fontSize: 20.0),
+          ),
+          //label: const Text('Skip'),
+          icon: Icon(
+            Icons.face_unlock_rounded,
+            size: 40,
+          ),
+          backgroundColor: Colors.deepPurple,
+        )),
+      ),
+    );
+  }
+}
